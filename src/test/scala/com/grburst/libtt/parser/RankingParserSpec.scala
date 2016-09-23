f446c8ccb4659e1ad3b3e50b2bef20408babf622
GRBurst <JuliusE89@gmail.com>
2016-09-23 17:22:34 +0200

    Add test for event detail parsing

--- src/test/scala/com/grburst/libtt/parser/RankingParserSpec.scala
+++ src/test/scala/com/grburst/libtt/parser/RankingParserSpec.scala
@@ -12,8 +12,8 @@ class RankingParserSpec extends org.specs2.mutable.Specification {
 
     val crp = RankingParser("src/test/res/myclub.htm")
     val lp = crp.get
-    lp(0) mustEqual Player(100100, "1 / 1000", 1000, "Max Mustermann", "Musterverein", 200100, 2222 )
-    lp(1) mustEqual Player(100200, "2 / 2000", 2000, "Maxi Musterfrau", "Musterverein", 200100, 2112 )
+    lp(0) mustEqual Player(100100, Some(1), Some(1000), "Max Mustermann", "Musterverein", 200100, Some(2222) )
+    lp(1) mustEqual Player(100200, Some(2), Some(2000), "Maxi Musterfrau", "Musterverein", 200100, Some(2112) )
 
   }
 
